467f47024214abe8b9467e5c209fca3b7b43b23d
DEDVO20 <dedvo2016@gmail.com>
2025-10-22 23:01:52 -0500

    terminacion modelo usuario

--- backend/src/models/Usuarios.ts
+++ backend/src/models/Usuarios.ts
@@ -1,5 +1,11 @@
 import { publicDecrypt } from "node:crypto";
-import { AssociationError, DataTypes, Model, Optional, Sequelize } from "sequelize";
+import {
+  AssociationError,
+  DataTypes,
+  Model,
+  Optional,
+  Sequelize,
+} from "sequelize";
 
 interface UsuarioAttributes {
   id: string;
@@ -17,9 +23,16 @@ interface UsuarioAttributes {
   actualizadoEn: Date;
 }
 
-interface UsuarioCreationAttributes extends Optional<UsuarioAttributes, "id" | "activo" | "creadoEn" | "actualizadoEn"> {}
+interface UsuarioCreationAttributes
+  extends Optional<
+    UsuarioAttributes,
+    "id" | "activo" | "creadoEn" | "actualizadoEn"
+  > {}
 
-class Usuario extends Model<UsuarioAttributes, UsuarioCreationAttributes> implements UsuarioAttributes {
+class Usuario
+  extends Model<UsuarioAttributes, UsuarioCreationAttributes>
+  implements UsuarioAttributes
+{
   public id!: string;
   public documento!: number;
   public nombre!: string;
@@ -33,79 +46,90 @@ class Usuario extends Model<UsuarioAttributes, UsuarioCreationAttributes> implem
   public activo!: boolean;
   public readonly creadoEn!: Date;
   public readonly actualizadoEn!: Date;
+
+  public static initModel(sequelize: Sequelize): typeof Usuario {
+    return Usuario.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes,
+          primaryKey: true,
+        },
+        documento: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        nombre: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        segundoNombre: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        primerApellido: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        segundoApellido: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        correoElectronico: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        nombreUsuario: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        contraseñaHash: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        areaId: {
+          type: DataTypes.UUID,
+          allowNull: false,
+        },
+        activo: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: true,
+        },
+        creadoEn: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+        actualizadoEn: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+      },
+      {
+        sequelize,
+        tableName: "usuarios",
+        timestamps: false,
+        underscored: true,
+      },
+    );
+  }
+  public static associate(models: any): void {
+    Usuario.belongsTo(models.Area, { foreignKey: "areaId", as: "area" });
+    Usuario.hasMany(models.Proceso, {
+      foreignKey: "creadoPor",
+      as: "procesosCreados",
+    });
+    Usuario.hasMany(models.NoConformidad, {
+      foreignKey: "detectadoPor",
+      as: "noConformidadesDetectadas",
+    });
+    Usuario.hasMany(models.Auditoria, {
+      foreignKey: "creadoPor",
+      as: "auditoriasCreadas",
+    });
+  }
 }
 
-public static initModel(sequelize: Sequelize): typeof Usuario {
-  return Usuario.init(
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes,
-        primaryKey: true
-      },
-      documento: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      nombre: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      segundoNombre: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      primerApellido: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      segundoApellido: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      correoElectronico: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      nombreUsuario: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      contraseñaHash: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      areaId: {
-        type: DataTypes.UUID,
-        allowNull: false
-      },
-      activo: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true
-      },
-      creadoEn: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-      },
-      actualizadoEn: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-      }
-    },
-    {
-      sequelize,
-      tableName: 'usuarios',
-      timestamps: false,
-      underscored: true,
-    }
-  );
-}
-public static associate(models: any): void {
-  Usuario.belongsTo(models.Area, { foreignKey: 'areaId', as: 'area' });
-  Usuario.hasMany(models.Proceso, { foreignKey: 'creadoPor', as:'procesosCreados' });
-  Usuario.hasMany(models.NoConformidad, { foreignKey: 'detectadoPor', as:'noConformidadesDetectadas' });
-  Usuario.hasMany(models.Auditoria, { foreignKey: 'creadoPor', as:'auditoriasCreadas' });
-}
+export default Usuario;
