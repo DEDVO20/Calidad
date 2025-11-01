6636df97d99f171976164a4b98a856210ba5eb01
MariaFer2006 <mafeherazoescobar@gmail.com>
2025-11-01 15:46:35 -0500

    rutas de Documentos Obsoletos y Pendientes

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -150,11 +150,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           },
           {
             title: "Aprobaciones Pendientes",
-            url: "#",
+            url: "/Aprobaciones_Pendientes",
           },
           {
             title: "Documentos Obsoletos",
-            url: "#",
+            url: "/Documentos_Obsoletos",
           },
         ],
       },
