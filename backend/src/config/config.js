386912793185c238da35039dc2c00a839ba7ef73
Sergio Carvajal <sergio.carvajal2003@gmail.com>
2026-01-20 18:08:20 -0500

    feat: vistas de Configuración y Seguridad (frontend)

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -4,7 +4,7 @@ require("ts-node").register({ transpileOnly: true });
 module.exports = {
   development: {
     username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "admin123",
+    password: process.env.DB_PASSWORD || "qwerty",
     database: process.env.DB_NAME || "calidad",
     host: process.env.DB_HOST || "localhost",
     port: parseInt(process.env.DB_PORT || "5432"),
